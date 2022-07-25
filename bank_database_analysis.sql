0ac02c7ebce4a9c42c8fecf9e0ea04512f243de7
Sourav Dey <106477719+deysourav287@users.noreply.github.com>
2022-07-25 21:45:30 +0530

    Update bank_database_analysis.sql

--- bank_database_analysis.sql
+++ bank_database_analysis.sql
@@ -132,17 +132,22 @@ select sum(balance) from bank_details
 select avg(balance) from bank_details
 /*try to find out who is having a min balance */
 select MIN(balance) from bank_details
+select * from bank_details order by balance limit 1
+select * from bank_details where balance in (select min(balance) from bank_details)
+select * from bank_details where balance = (select min(balance) from bank_details)
 /*try to find out who is having a mazxmim balance */
 select MAX(balance) from bank_details
+select * from bank_details order by balance desc limit 1
+select * from bank_details where balance = (select max(balance) from bank_details)
 /*try to prepare a list of all the person who is having loan */
 select * from bank_details where loan = "yes"
 /*try to find out average balance for all the people whose job role is admin */
 select avg(balance) from bank_details where job = "admin." 
 /*try to find out a record  without job whose age is below 45 */
-select age from bank_details where age < 45
+select * from bank_details where age < 45 and job = "unknown"
 /*try to find out a record where education is primarty and person is jobless*/
-select * from bank_details where education = "primary" and job = "retired"
+select * from bank_details where education = "primary" and job = "unknown"
 /*try to find of a record whose bank account is having a negative balance*/
 select * from bank_details where balance < 0
 /* try to find our a record who is not having house at all along with there balance */
-select * from bank_details where balance = 0 and housing = "no"
+select housing, balance from bank_details where  housing = "no"
